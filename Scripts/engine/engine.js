1b31bd8433c229ec2301dc4b72440b79b10f99bb
Arvind <arvin09@gmail.com>
2014-01-05 20:08:16 +0530

    Integrated the menu witht the shell - Menu is now configured to work with module or topics - Menu interaction also updates Pagination and next previous interaction.

--- Scripts/engine/engine.js
+++ Scripts/engine/engine.js
@@ -1,12 +1,14 @@
 //TODO : Derive this from config.xml
 var EnvVariables = {
 		paths : {
-					'Data' : 'Data',
-					'Templates' : 'Templates',
-					'EngineTemplate' : 'Scripts/engine/html'
-				},
+			'Data' : 'Data',
+			'Templates' : 'Templates',
+			'EngineTemplate' : 'Scripts/engine/html/',
+			'EngineImage' : 'Scripts/engine/image/'
+		},
 		'lang' : 'eng'
 };
+
 var Engine = (function(){
 	var courseStructure = null;
 	var USERSTATE = {
@@ -14,26 +16,27 @@ var Engine = (function(){
 			topic : 0
 	};
 	var templatesCache = {
-			
+
 	};
 	var topicDataCache = {
-			
+
 	};
-	
+
 	function getCourseStructure(){
 		return $.ajax({
 			url : EnvVariables.paths['Data'] + "/" +EnvVariables['lang']+  '/structure.xml',  
 			success : function(response){
 				courseStructure = xml2jsonObj(response.childNodes[0]);
+				console.log("Structure " , courseStructure);
 			}
 		});
 	};
-	
-	
+
+
 	function renderTopic(template, topicData){
 		$(".template-conatiner").html( Handlebars.compile(template)(topicData));
 	};
-	
+
 	function getTemplateData(templateId,templateType){
 		if(templatesCache[templateId]){
 			var differed = $.Deferred();
@@ -41,14 +44,14 @@ var Engine = (function(){
 			return differed.resolve();
 		}
 		return $.ajax({
-			url : templateType !== "engine" ? EnvVariables.paths['Templates'] + "/" +EnvVariables['lang']+  "/" + templateId : EnvVariables.paths['EngineTemplate'] + "/" + templateId,  
-			success : function(response){
-				templatesCache[templateId] = response;
-			}
+			url : templateType !== "engine" ? EnvVariables.paths['Templates'] + "/" +EnvVariables['lang']+  "/" + templateId : EnvVariables.paths['EngineTemplate']  + templateId,  
+					success : function(response){
+						templatesCache[templateId] = response;
+					}
 		});
 	};
-	
-	
+
+
 	function getTopicData(topicDataId){
 		if(topicDataCache[topicDataId]){
 			console.log('returning differed getTopicData');
@@ -62,18 +65,17 @@ var Engine = (function(){
 			}
 		});
 	};
-	
+
 	showTopic =  function(){
 		var topic = courseStructure.course.module[USERSTATE.module].topic[USERSTATE.topic];
 
-
 		var topicTemplateId = topic.screen['@templateID'];
 		var templateDataId = topic.screen['@xmlName'];
-		
+
 		var templatePromise = getTemplateData(topicTemplateId);
 		var topicDataPromise = getTopicData(templateDataId);
-		
-		
+
+
 		return $.when(templatePromise, topicDataPromise).then(function(){
 			var template = templatesCache[topicTemplateId];
 			var topicData = topicDataCache[templateDataId];
@@ -81,94 +83,133 @@ var Engine = (function(){
 			renderTopic(template, topicData);
 		});
 	};
-	
+
 	registerEvents = function(){
-		
+
 		$("ul.inline-list").on("mouseenter","img",function(){
 			return rollOverHandler.call(this);
 		});
-		
+
 		$("ul.inline-list").on("mouseleave","img",function(){
 			return rolloutHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='playpause']").on("click", function() {
 			return playpauseHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='menu']").on("click", function() {
 			return menuHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='glossary']").on("click", function() {
 			return glossaryHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='prev']").on("click", function() {
 			return prevHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='next']").on("click", function() {
 			return nextHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='help']").on("click", function() {
 			return nextHandler.call(this);
 		});
-		
+
 		$("ul.inline-list li[id='exit']").on("click", function() {
 			return exitHandler.call(this);
 		});
+
+		$(".accordion").on("click","li",function(){
+			return topicHandler.call(this);
+		});
+
+		$(".accordion").on("click","a",function(){
+			return moduleHandler.call(this);
+		});
+
+	};
+
+	moduleHandler = function(){
+		console.log("Module ID - ", $(this).attr("class"));
+		var menuType = courseStructure.course["@menuType"];
+		if(menuType === "module"){
+			USERSTATE.module = parseInt($(this).attr("class"));
+			USERSTATE.topic = 0;
+			$('#menu-panel').foundation('reveal', 'close');
+			showTopic();
+			updatePagination();
+			return false;
+		}
 		
 	};
-	
+
+	topicHandler = function(){
+
+		USERSTATE.module =  parseInt($(this).attr("class").split("-")[0]);
+		USERSTATE.topic =  parseInt($(this).attr("class").split("-")[1]);
+		$('#menu-panel').foundation('reveal', 'close');
+		showTopic();
+		updatePagination();
+	};
+
+	updatePagination = function(){
+
+		var modules = courseStructure.course.module;
+		var topics = modules[USERSTATE.module].topic;
+		$('.curr-page').text(USERSTATE.topic+1);
+		$('.total-page').text(topics.length);
+	};
+
 	rollOverHandler = function(){
 		var srcName = $(this).attr("src").split(".")[0].split("/")[3];
-		var imageSrc = "Scripts/engine/image/" + srcName + "_over.png";
+		var imageSrc = EnvVariables.paths['EngineImage'] + srcName + "_over.png";
 		$(this).attr("src", imageSrc);
 	};
-	
+
 	rolloutHandler = function(){
 		var srcName = $(this).attr("src").split(".")[0].split("/")[3].split("_")[0];
-		var imageSrc = "Scripts/engine/image/" + srcName + ".png";
+		var imageSrc = EnvVariables.paths['EngineImage'] + srcName + ".png";
 		$(this).attr("src", imageSrc);
 	};
-	
+
 	playpauseHandler = function () {
 		var imgObj = $(this).find('img');
 		var status = $(imgObj).attr("src").split(".")[0].split("/")[3]
-				.split("_")[0];
+		.split("_")[0];
 		status === "play" ? $(imgObj).attr({
-			"src" : "Scripts/engine/image/pause.png",
+			"src" : EnvVariables.paths['EngineImage'] + "pause.png",
 			"title" : "Pause"
 		}) : $(imgObj).attr({
-			"src" : "Scripts/engine/image/play.png",
+			"src" : EnvVariables.paths['EngineImage'] + "play.png",
 			"title" : "Play"
 		});
 	};
-	
+
 	menuHandler = function(){
 		console.log("Menu Click");
 	};
-	
+
 	glossaryHandler = function(){
 		console.log("Glossary Click");
 	};
-	
+
 	prevHandler = function(){
 		console.log("Previous Click");
 		showPrevPage();
 	};
-	
+
 	nextHandler = function(){
 		console.log("Next Click");
 		showNextPage();
 	};
-	
+
 	helpHandler = function(){
 		console.log("Help Click");
 	};
-	
+
 	exitHandler = function(){
 		console.log("Exit Click");
 		var bool =  confirm("Are you sure you want to exit");
@@ -176,22 +217,16 @@ var Engine = (function(){
 			//TODO: unload window 
 		}
 	};
-	
-	updatePagination = function(){
-		
-		var modules = courseStructure.course.module;
-		var topics = modules[USERSTATE.module].topic;
-		$('.curr-page').text(USERSTATE.topic+1);
-		$('.total-page').text(topics.length);
-	};
-	
+
+
+
 	showNextPage = function(){
 		var modules = courseStructure.course.module;
 		var currentModule = modules[USERSTATE.module];
 		var currentTopic = modules[USERSTATE.module].topic[USERSTATE.topic];
-		
+
 		var nextTopic = ++USERSTATE.topic ;
-		
+
 		var moudlesLength = modules.length;
 		if(nextTopic > currentModule.topic.length-1){
 			USERSTATE.topic = nextTopic = currentModule.topic.length-1;
@@ -202,9 +237,9 @@ var Engine = (function(){
 			}
 			USERSTATE.topic = nextTopic = 0;
 		}
-		
-		
-		
+
+
+
 		var topics = modules[USERSTATE.module].topic;
 		if(USERSTATE.module === moudlesLength-1 &&  USERSTATE.topic === topics[USERSTATE.topic].length-1){
 			//TODO disable next button
@@ -214,7 +249,7 @@ var Engine = (function(){
 		showTopic();
 		updatePagination();
 	};
-	
+
 	initView = function(){
 		var modules = courseStructure.course.module;
 		var topics = modules[USERSTATE.module].topic;
@@ -222,9 +257,9 @@ var Engine = (function(){
 		$('.total-page').text(topics.length);
 		generateMenu(modules);
 	};
-	
+
 	generateMenu = function(modules){
-		
+
 		var moduleArray=[];		
 		for(var i=0;i<modules.length;i++){
 			var moduleObj= {
@@ -232,43 +267,36 @@ var Engine = (function(){
 					name: "",
 					topics:[],
 			};
-			
-			moduleObj.id = "panel"+(i+1);
+
+			moduleObj.id = i;
 			moduleObj.name = modules[i]["@title"];
 			var topicLength = modules[i].topic.length;
-			
+
 			for(var j=0;j<topicLength;j++){
 				var topicObj = {
+						id:'',
 						name:''
 				};
-				
+				topicObj.id = i+"-"+j;
 				topicObj.name = modules[i].topic[j]["@title"];
-				
 				moduleObj.topics.push(topicObj);
 			}
-			
+
 			moduleArray.push(moduleObj);
 		}
-		
-		console.log(moduleArray);
-		
-		//console.log("Engine Menu Template",.responseText);
-		
+
 		var templatePromise = getTemplateData("menuTemplate.html","engine");
 		$.when(templatePromise).then(function(){
 			var template = templatesCache["menuTemplate.html"];
 			$(".accordion").append(Handlebars.compile(template)(moduleArray));
 		});
-		
-		
-		
 	};
-	
-	
+
+
 	showPrevPage = function(){
 		var currentModule = courseStructure.course.module[USERSTATE.module];
 		var currentTopic = courseStructure.course.module[USERSTATE.module].topic[USERSTATE.topic];
-		var prevTopic = --USERSTATE.topic ;
+		var prevTopic = --USERSTATE.topic;
 		if(prevTopic < 0){
 			USERSTATE.topic = prevTopic = 0;
 			USERSTATE.module--; 
@@ -278,18 +306,18 @@ var Engine = (function(){
 			}
 			USERSTATE.topic = prevTopic = courseStructure.course.module[USERSTATE.module].topic.length - 1;
 		}
-		
+
 		if(USERSTATE.module === 0 && USERSTATE.topic===0){
 			//TODO disable back button
 		} else {
 			//TODO enable back button
 		}
-		
+
 		showTopic();
 		updatePagination();
 	};
-	
-	
+
+
 	return {
 		initialize : function(){
 			var courseStructureObtained = getCourseStructure();
@@ -297,11 +325,11 @@ var Engine = (function(){
 				showTopic();
 				initView();
 			});
-			
+
 			registerEvents();
 		},
-		
-		
+
+
 	}
 })();
 
