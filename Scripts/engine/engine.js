abd180223a18be8b5de7f9e25ab65f53c282468b
varonde <vishwanath.arondekar@morningstar.com>
2014-01-17 17:31:36 +0530

    remove hardcoding.

--- Scripts/engine/engine.js
+++ Scripts/engine/engine.js
@@ -1039,7 +1039,7 @@ var Engine = (function(){
 				
 				
 				doLMSInitialize();
-				var scormString = "1,1|1,1,1|1,1,0~2,0,1~0,0,0"; //getSCORMData();
+				var scormString = getSCORMData();
 				updateCourseState(scormString);
 				showTopic();
 				initView();
